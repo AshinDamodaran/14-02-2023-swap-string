4ae50adbc32becb0ec9354ba2c2bf743e5c861b7
ashin.damodaran <ashin.damodaran@ASHINDAMODARAN>
2023-02-14 16:22:55 +0530

    second

--- Reverse stringer/Program.cs
+++ Reverse stringer/Program.cs
@@ -1,5 +1,5 @@
-﻿        
-          Console.Write("Enter a String : ");
+﻿using System;
+Console.Write("Enter a String : ");
 string originalString = Console.ReadLine();
 string reverseString = string.Empty;
 for (int i = originalString.Length - 1; i >= 0; i--)
@@ -8,4 +8,3 @@ for (int i = originalString.Length - 1; i >= 0; i--)
 }
 Console.Write($"Reverse String is : {reverseString} ");
 Console.ReadLine();
-//
